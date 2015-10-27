ef2876b4b46068d00e91d6cd3985ebe91f29f4f0
Iran_Syber <iran.syber@gmail.com>
2015-10-27 10:29:52 -0300

    Adição do dbContext

---
+++ SystemDevelopment/SystemDevelopment/Contexto/DBcontexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+
+namespace SystemDevelopment.Contexto
+{
+    public class DBcontexto : DbContext
+    {
+        public DBcontexto(string stringConexao)
+            : base(stringConexao)
+        { 
+        }
+    }
+}
