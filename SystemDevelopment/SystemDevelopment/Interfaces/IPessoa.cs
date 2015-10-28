a2b8db079d469e5a9fefe04d6aec212e51bca386
Iran_Syber <iran.syber@gmail.com>
2015-10-28 13:38:21 -0300

    Criação da pagina de Usuarios

---
+++ SystemDevelopment/SystemDevelopment/Interfaces/IPessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SystemDevelopment.Interfaces
+{
+    public interface IPessoa
+    {
+        IEnumerable<Models.Data1.Pessoa> GetAllPessoa();
+    }
+}
